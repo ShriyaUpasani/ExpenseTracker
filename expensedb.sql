3d9f710a38dd2992cdb905ea4ec2a94377723bbb
Shriya Upasani <75808352+ShriyaUpasani@users.noreply.github.com>
2021-06-30 15:38:48 +0530

    Add files via upload

---
+++ expensedb.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 30, 2021 at 12:06 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `expensedb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblexpense`
+--
+
+CREATE TABLE `tblexpense` (
+  `ID` int(10) NOT NULL,
+  `UserId` int(10) NOT NULL,
+  `ExpenseDate` date DEFAULT NULL,
+  `ExpenseItem` varchar(200) DEFAULT NULL,
+  `ExpenseCost` varchar(200) DEFAULT NULL,
+  `NoteDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblexpense`
+--
+
+INSERT INTO `tblexpense` (`ID`, `UserId`, `ExpenseDate`, `ExpenseItem`, `ExpenseCost`, `NoteDate`) VALUES
+(2, 1, '2021-06-29', 'curd', '55', '2021-06-30 09:39:36'),
+(3, 1, '2021-06-30', 'plates', '1000', '2021-06-30 09:40:11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbluser`
+--
+
+CREATE TABLE `tbluser` (
+  `ID` int(10) NOT NULL,
+  `FullName` varchar(150) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Password` varchar(200) DEFAULT NULL,
+  `RegDate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbluser`
+--
+
+INSERT INTO `tbluser` (`ID`, `FullName`, `Email`, `MobileNumber`, `Password`, `RegDate`) VALUES
+(1, 'Shriya Upasani', 'shriyamauli@gmail.com', 7045059567, '48bb6e862e54f2a795ffc4e541caed4d', '2021-06-29 22:38:49');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tblexpense`
+--
+ALTER TABLE `tblexpense`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tbluser`
+--
+ALTER TABLE `tbluser`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tblexpense`
+--
+ALTER TABLE `tblexpense`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbluser`
+--
+ALTER TABLE `tbluser`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
